1d01ff1a82ca3fe6f331fadd9797c584b71c4965
Gonzalo1996 <gonald96@gmail.com>
2022-06-09 16:46:28 -0300

    Creación del proyecto.

---
+++ AppEjercicio2_Grupo10/src/main/java/principal/Main.java
@@ -0,0 +1,24 @@
+package principal;
+
+import entidad.Usuario;
+import negocio.UsuarioNegocio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Usuario usuario = new Usuario();
+	    usuario.setUsuario("Pepe");
+	    usuario.setContrasenia("123");
+	    
+	    UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+	    boolean estado= usuarioNegocio.agregarUsuario(usuario);
+	    if(estado)
+	    	System.out.println("Se agrego correctamente");
+	    else
+	    	System.out.println("No se pudo agregar, el usuario ya existe en la BD");
+	    
+	    System.out.println("Hola Mundo 2.");
+		    	
+	}	
+}
